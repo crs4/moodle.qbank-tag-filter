6240558b5a09782d931e65b71b654ece8fbffda6
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-10 17:00:13 +0200

    Added file for managing plugin versioning

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2015101500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2015050500;        // Requires this Moodle version.
+$plugin->component = 'local_questionbanktagfilter'; // Full name of the plugin (used for diagnostics).
+$plugin->cron      = 0;
