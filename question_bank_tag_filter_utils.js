a117d3164f0ef499acefeab1b6c585e7d3f2e316
Marco Enrico Piras <kikkomep@crs4.it>
2016-01-29 08:23:06 +0100

    Cleaning

--- question_bank_tag_filter_utils.js
+++ question_bank_tag_filter_utils.js
@@ -4,22 +4,9 @@
 
 M.question_bank_tag_filter_helper = {};
 
-
 /**
- *  echo '<script type="text/javascript">document.getElementById("search_by_tag_button").onclick=';
- echo 'function(){ console.log(this); ';
-        echo 'var s = document.getElementById("id_selectatag"); console.log(s);';
-        echo 'var tags = [];';
-        echo 'for(var i=0; i < s.options.length; i++){ ';
-        echo 'console.log(i, s.options[i],  s.options[i].selected);';
-        echo 'if(s.options[i].selected){ tags.push(s.options[i].value)}; } ';
-        echo 'var tags_input_value = tags.join(",");';
-        echo 'console.log(tags_input_value, tags);';
-        echo 'document.getElementById("id_tags").value=tags_input_value; ';
-        echo 'alert("check:" + tags_input_value); document.getElementById("displayoptions").submit() }';
- echo '</script>';
+ * Initializes the module
  */
-
 M.question_bank_tag_filter_helper.init = function () {
 
     // Registers the click event
@@ -36,7 +23,6 @@ M.question_bank_tag_filter_helper.init = function () {
         var tags_input_value = tags.join(",");
         console.log(tags_input_value, tags);
         document.getElementById("id_tags").value = tags_input_value;
-        //alert("check:" + tags_input_value);
         document.getElementById("displayoptions").submit();
     }
-}
+};
