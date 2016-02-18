a7edea08b5aee855d64519f386898c722e42f3d2
Marco Enrico Piras <kikkomep@crs4.it>
2016-02-18 17:04:48 +0100

    Updated version number

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016012500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2016021800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015051100;        // Requires this Moodle version.
 $plugin->component = 'local_questionbanktagfilter'; // Full name of the plugin (used for diagnostics).
 $plugin->cron      = 0;
